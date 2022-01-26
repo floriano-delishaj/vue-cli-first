1e0cf199a9065b3f99c2060d8a4efa6df7c57936
floriano-delishaj <delishaj.floriano@live.it>
2022-01-26 15:57:52 +0100

    end project + bonus jumbotron

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,23 @@
   <div id="app">
     
     <Title />
-    
+    <Jumbotron />
+
   </div>
 </template>
 
 <script>
 import Title from './components/Title.vue'
+import Jumbotron from './components/Jumbo.vue'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
 
 export default {
   name: 'App',
   components: {
-    Title
-  }
+    Title,
+    Jumbotron
+  },
 }
 </script>
 
---
+++ src/components/Jumbo.vue
@@ -0,0 +1,47 @@
+<template>
+    
+    <div class="jumbotron d-flex align-items-center">
+        <div class="mx-5 jumbo-content">
+            <h2>
+                Un'avventura cooperativa selvaggia ti attende
+            </h2>
+
+            <div v-if="modal" class="btn btn-success mt-2">
+                Per info
+            </div>
+
+            <p class="pt-3">
+                Trova un amico e partite insieme per l'avventura più folle delle vostre vite in It Takes Two, un gioco d'avventura cooperativa a piattaforme che stravolge il suo genere.
+            </p>
+
+            <div class="btn btn-warning mt-2">
+                Acquista ora!
+            </div>
+
+        </div>
+    </div>
+
+</template>
+
+
+
+<script>
+
+</script>
+
+
+<style>
+
+.jumbotron {
+    background: url(../assets/img-1.jpg);
+    height: 700px;
+}
+
+.jumbo-content {
+    width: 20%;
+    text-align: left;
+    color: #fff
+}
+
+
+</style>
